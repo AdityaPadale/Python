5c487a32bff5b304e3771eb13e96dbd8831fbb1b
Aditya <160272027+AdityaPadale@users.noreply.github.com>
2025-07-28 19:07:40 +0530

    Create Hello_Vehicles.py
    Basic code in unique way

---
+++ Hello_Vehicles.py
@@ -0,0 +1,9 @@
+# Hello World is outdated for me 
+# lets go with a situation where you have a fleet of vehicles. You want to write a basic Python program that just prints info about a vehicle in which you have entered in vehicle and it displays on screen
+
+
+print("Welcome to vehical tracker")
+print("Vehicle name: Tata Safari")
+print("Registration Year: 2003")
+print("Fuel Level: 80% Full")
+print("Achievment type: Stock Market Boom")
